0354db55e6b3bc091681276122b9649bdfc95f0f
mdunser <marc.dunser@cern.ch>
2014-10-08 10:38:54 +0200

    added go.py, which runs un-analyzed pedestal runs automatically

---
+++ go.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from RunInfo import RunInfo
+import math
+from subprocess import call
+
+RunInfo.load('runs.json')
+
+
+ped_not = []
+ped_all = []
+
+for rn, r in RunInfo.runs.items():
+    if r.data_type == 1:
+        ped_all.append(rn)
+        if math.isnan(r.pedestal):
+            ped_not.append(rn)
+
+
+print 'these are the unanalyzed pedestal runs:'
+print ped_not
+
+for run in ped_not:
+    call('echo python Analyze.py '+str(run), shell=True)
