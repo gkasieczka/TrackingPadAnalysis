8ba1c7976def39bc9c50187cc85a0618182000fb
Felix Bachmair <bachmair@phys.ethz.ch>
2015-02-21 13:49:23 +0100

    addded make_area_time_plot

--- AnalyzeHelpers.py
+++ AnalyzeHelpers.py
@@ -4,33 +4,34 @@
 
 import ROOT, copy
 from array import array
+import math
 from ROOT import RooFit, RooRealVar, RooGaussian, RooLandau, RooDataSet, RooArgList, RooTreeData, RooFFTConvPdf, RooDataHist
 
 
 def irr(diamond):
-    if diamond in ['IIa-2', 'IIa-3', 'S30']:
+    if diamond in ['IIa-2', 'IIa-3', 'S30','2A87-E']:
         return 'n-irr.'
     if diamond in ['S129', 'IIa-1', 'IIa-5']:
         return 'non-irr.'
     if diamond in ['S125', 'S66']:
         return 'p-irr.'
     else:
-        return 'foobar'
+        return 'unknown'
 
 
 def addDiamondInfo(x1, y1, x2, y2, my_run):
-    pave = ROOT.TPaveText(x1, y1, x2, y2, 'NDC')
+    pave = ROOT.TPaveText(x1, y1, x2, y2, 'NDC NB')
     pave.SetTextAlign(12)
     pave.SetTextFont(82)
     pave.SetFillColor(0)
-    pave.AddText(my_run.diamond+'\t'+irr(my_run.diamond))
+    pave.SetFillStyle(0)
+    pave.AddText(my_run.diamond+',\t'+irr(my_run.diamond)+', run %s'%my_run.number)
     pave.AddText('bias: '+str(my_run.bias_voltage)+' V')
-    # pave.AddText('rate: '+rate)
     pave.AddText('rate: %s' %(my_run.get_rate_string()))
     pave.SetShadowColor(0)
     pave.SetBorderSize(0)
     pave.SetTextFont(42)
-    pave.SetTextSize(0.04)
+    pave.SetTextSize(0.025)
     return pave
 
 def median(ls):
@@ -214,3 +215,77 @@ def fitLandauGaus(hist, full = False):
     # frame.Draw()
     # c.SaveAs('histograms/outputhisto'+hist.GetName().split('pz')[1]+'.pdf')
     return dh, copy.deepcopy(a), copy.deepcopy(frame),copy.deepcopy(hist2)
+
+
+def make_area_time_plot(h_signal, h_entries, xpos,ypos,radius,descr=''):
+    if descr != '':
+        name = 'hSignalTime_%s_x%03d_y%03d_r%03d'%(descr,xpos*100,ypos*100,radius*100)
+    else:
+        name = 'hSignalTime_x%03d_y%03d_r%03d'%(xpos*100,ypos*100,radius*100)
+    title = 'Signal vs. Time: %4.1d/%4.1f, r = %2.1f'%(xpos,ypos,radius)
+    bins = h_signal.GetNbinsZ()
+    zmin = h_signal.GetZaxis().GetXmin()
+    zmax = h_signal.GetZaxis().GetXmax()
+    signal = ROOT.TH1F(name,title,bins,zmin,zmax)
+    name = 'hSignalEntriesTime_x%03d_y%03d_r%03d'%(xpos*100,ypos*100,radius*100)
+    entries = ROOT.TH1F(name,title,bins,zmin,zmax)
+    for xbin in range(1,h_signal.GetNbinsX()+1):
+        for ybin in range(1,h_signal.GetNbinsY()+1):
+            x = h_signal.GetXaxis().GetBinCenter(xbin)
+            y = h_signal.GetYaxis().GetBinCenter(ybin)
+            #check if bin within the radius
+            xx = x - xpos
+            yy = y - ypos
+            # print '%2.4f/%2.4f %2.4f/%2.4f %2.4f/%2.4f %2.4f %2.4f => %d'%(x,y,xpos,ypos,xx,yy,(xx)**2+(yy)**2,radius**2,(xx)**2+(yy)**2 > radius**2)
+            if (xx)**2+(yy)**2 > radius**2:
+                continue
+            h_signal.GetXaxis().SetRange(xbin, xbin)
+            h_signal.GetYaxis().SetRange(ybin, ybin)
+            h_entries.GetXaxis().SetRange(xbin, xbin)
+            h_entries.GetYaxis().SetRange(ybin, ybin)
+            proj_signal = h_signal.Project3D('z')
+            proj_entries = h_entries.Project3D('z')
+            signal.Add(proj_signal,1)
+            entries.Add(proj_entries,1)
+
+    signal.GetXaxis().SetTitle('minutes')
+    signal.Divide(entries)
+    for bin in range(1,signal.GetNbinsX()+1):
+        nentries = entries.GetBinContent(bin)
+        if nentries == 0: nentries = 1
+        signal.SetBinError(bin,signal.GetBinContent(bin)/math.sqrt(nentries))
+
+    signal_rel = copy.deepcopy(signal)
+    if signal[1] !=0:
+        signal_rel.Scale(1. / signal[1])
+    signal_rel.GetZaxis().SetRangeUser(0, 1.05)
+    return signal,entries,signal_rel
+
+def get_mean_and_sigma(sample):
+    if len(sample) == 0:
+        mean = 0
+        variance = 0
+    else:
+        mean = float(reduce(lambda x,y : x+y, sample)) / len(sample)
+        variance = reduce(lambda x,y: x+y, map(lambda xi: (xi-mean)**2, sample))/ len(sample)
+    return mean, math.sqrt(variance)
+
+def find_range_3d(signal):
+    signals = []
+    print signal.GetXaxis().GetFirst(),signal.GetXaxis().GetLast()
+    print signal.GetYaxis().GetFirst(),signal.GetYaxis().GetLast()
+    for xbin in range(signal.GetXaxis().GetFirst(),signal.GetXaxis().GetLast()):
+        for ybin in range(signal.GetYaxis().GetFirst(),signal.GetYaxis().GetLast()):
+            signals.append(signal.GetBinContent(xbin,ybin))
+    l = len(signals)
+    signals = sorted(s for s in signals if s > 5)
+    signals = signals[int(l*.05):int(l*.95)]
+
+    xlow,xup =  signals[int(l*.05)],signals[-2]
+    mean,sigma = get_mean_and_sigma(signals)
+    if xup>mean + 4 *sigma:
+        xup = mean + 4 *sigma
+    if xlow<mean - 4 *sigma:
+        xlow = mean - 4 *sigma
+    print xlow,xup
+    return xlow,xup
